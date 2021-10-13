ccae91f9c61bcfc253a736fbc2d85458f003b88f
김기수 <gimgisu@gimgisuui-MacBookPro.local>
2021-10-13 16:15:55 +0900

    Initial commit

---
+++ src/main/java/com/deeplify/tutorial/batch/jobs/TutorialConfig.java
@@ -0,0 +1,31 @@
+package com.deeplify.tutorial.batch.jobs;
+
+import com.deeplify.tutorial.batch.tasklets.TutorialTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class TutorialConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job tutorialJob() {
+        return jobBuilderFactory.get("tutorialJob")
+                .start(tutorialStep())  // Step 설정
+                .build();
+    }
+
+    @Bean
+    public Step tutorialStep() {
+        return stepBuilderFactory.get("tutorialStep")
+                .tasklet(new TutorialTasklet()) // Tasklet 설정
+                .build();
+    }
+}
